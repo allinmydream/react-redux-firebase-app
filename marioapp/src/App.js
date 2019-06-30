e1743f6477b6d352dda4b4bd7306f95273b4f9de
allinmydream <jade.d.park7@gmail.com>
2019-07-01 00:29:28 +0900

    SignInLinks & SignOutLinks 컴포넌트 생성

--- marioapp/src/App.js
+++ marioapp/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/NavBar';
 
 
 class App extends Component {
   render() {
     return(
-      <div className="App">
-        <h1>MarioPlan</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+        </div>
+      </BrowserRouter>
     )
   }
 }
