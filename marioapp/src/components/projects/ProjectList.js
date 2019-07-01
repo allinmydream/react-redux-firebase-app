afc247cdb956195d05c9d3fcbdfcb4b9f8b1b80a
happyjade <jade.d.park@gmail.com>
2019-07-01 13:59:37 +0900

    Projectlist 와 Notifications 위치 잡고 컴포넌트 작성

---
+++ marioapp/src/components/projects/ProjectList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            
+            <div className="card z-depth-0 project-su">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Daeun</p>
+                    <p className="grey-text">first July, 1pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-su">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Daeun</p>
+                    <p className="grey-text">first July, 1pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-su">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Daeun</p>
+                    <p className="grey-text">first July, 1pm</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProjectList
